c04d4bf258b684500d9f6df0789c8dc2a221a642
bed-com-31-19 <bed-com-31-19@unima.ac.mw>
2022-12-29 05:06:55 +0200

    implementing header

---
+++ src/components/Header.js
@@ -0,0 +1,148 @@
+import React from 'react';
+import "./Header.scss";
+import { NavLink, Link } from 'react-router-dom';
+import SearchIcon from "@mui/icons-material/Search";
+
+const Header = () => {
+  return (
+    <>
+      <header className="header-top-strip py-3">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-6">
+              <p className="text-white mb-0">
+                Free shipping over K10,000 & free returns
+              </p>
+            </div>
+            <div className="col-6">
+              <p className="text-end text-white mb-0">
+                HotLine:{" "}
+                <a className="text-white" href="tel:+265 888 234 314">
+                  +265 994 149 258
+                </a>{" "}
+              </p>
+            </div>
+          </div>
+        </div>
+      </header>
+      <header className="header-upper py-3">
+        <div className="container-xxl">
+          <div className="row align-items-center">
+            <div className="col-2">
+              <h4>
+                <Link className="text-white">UNIMA MARKET</Link>
+              </h4>
+            </div>
+            <div className="col-5">
+              <div class="input-group">
+                <input
+                  type="text"
+                  class="form-control py-2"
+                  placeholder="Search product here"
+                  aria-label="Search product here"
+                  aria-describedby="basic-addon2"
+                />
+                <span class="input-group-text p-2.5" id="basic-addon2">
+                  <SearchIcon className="fs-6" />
+                </span>
+              </div>
+            </div>
+            <div className="col-5">
+              <div className="header-upper-links d-flex align-items-center justify-content-between">
+                <div>
+                  <Link className="d-flex align-items-center gap-10 text-white">
+                    <img src="/images/compare.svg" alt="compare" />
+                    <p className="mb-0">
+                      compare <br /> products
+                    </p>
+                  </Link>
+                </div>
+                <div>
+                  <Link className="d-flex align-items-center gap-10 text-white">
+                    <img src="/images/wishlist.svg" alt="wishlist" />
+                    <p className="mb-0">
+                      favorite <br /> wishlist
+                    </p>
+                  </Link>
+                </div>
+                <div>
+                  <Link className="d-flex align-items-center gap-10 text-white">
+                    <img src="/images/user.svg" alt="login" />
+                    <p className="mb-0">
+                      log in <br /> my account
+                    </p>
+                  </Link>
+                </div>
+                <div>
+                  <Link className="d-flex align-items-center gap-10 text-white">
+                    <img src="/images/cart.svg" alt="" />
+                    <div className="d-flex flex-column gap-10">
+                      <span className="badge bg-white text-dark">0</span>
+                      <p className="mb-0">K50,000</p>
+                    </div>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+      <header className="header-bottom py-3">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12">
+              <div className="menu-bottom d-flex align-items-center gap-30">
+                <div>
+                  <div className="dropdown">
+                    <button
+                      className="btn btn-secondary dropdown-toggle bg-transparent border-0 gap-15 d-flex align-items-center gap-30"
+                      type="button"
+                      id="dropdownMenuButton1"
+                      data-bs-toggle="dropdown"
+                      aria-expanded="false"
+                    >
+                      <img src="/images/menu.svg" alt="more" />
+                      <span className="me-5 d-inline-block">
+                        Shop Categories
+                      </span>
+                    </button>
+                    <ul
+                      className="dropdown-menu"
+                      aria-labelledby="dropdownMenuButton1"
+                    >
+                      <li>
+                        <Link className="dropdown-item text-white" href="#">
+                          Action
+                        </Link>
+                      </li>
+                      <li>
+                        <Link className="dropdown-item text-white" href="#">
+                          Another action
+                        </Link>
+                      </li>
+                      <li>
+                        <Link className="dropdown-item text-white" href="#">
+                          Something else here
+                        </Link>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+                <div className="menu-links">
+                  <div className="d-flex align-items-center gap-15">
+                    <NavLink to="/">Home</NavLink>
+                    <NavLink to="/">Our Store</NavLink>
+                    <NavLink to="/">Blogs</NavLink>
+                    <NavLink to="/contact">Contact</NavLink>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+}
+
+export default Header;
